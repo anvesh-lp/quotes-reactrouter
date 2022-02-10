76bb8c6a6eb4bdd0eea266823fb07d589478dd76
anvesh-lp <knanvesh.01@gmail.com>
2022-02-10 12:44:34 -0500

    Adding prompt to form validation

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,47 +1,60 @@
-import { useRef } from 'react';
+import {Fragment, useRef, useState} from 'react';
 
 import Card from '../UI/Card';
 import LoadingSpinner from '../UI/LoadingSpinner';
 import classes from './QuoteForm.module.css';
+import {Prompt} from "react-router-dom";
 
 const QuoteForm = (props) => {
-  const authorInputRef = useRef();
-  const textInputRef = useRef();
+    const authorInputRef = useRef();
+    const textInputRef = useRef();
+    const [isEntering, setIsEntering] = useState(false);
 
-  function submitFormHandler(event) {
-    event.preventDefault();
+    function submitFormHandler(event) {
+        event.preventDefault();
 
-    const enteredAuthor = authorInputRef.current.value;
-    const enteredText = textInputRef.current.value;
+        const enteredAuthor = authorInputRef.current.value;
+        const enteredText = textInputRef.current.value;
 
-    // optional: Could validate here
+        // optional: Could validate here
 
-    props.onAddQuote({ author: enteredAuthor, text: enteredText });
-  }
+        props.onAddQuote({author: enteredAuthor, text: enteredText});
+    }
 
-  return (
-    <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
-        {props.isLoading && (
-          <div className={classes.loading}>
-            <LoadingSpinner />
-          </div>
-        )}
+    const formFocusHandler = () => {
+        setIsEntering(true);
+    }
+    const submitButtonHandler = () => {
+        setIsEntering(false)
+    }
 
-        <div className={classes.control}>
-          <label htmlFor='author'>Author</label>
-          <input type='text' id='author' ref={authorInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='text'>Text</label>
-          <textarea id='text' rows='5' ref={textInputRef}/>
-        </div>
-        <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
-        </div>
-      </form>
-    </Card>
-  );
+    return (
+        <Fragment>
+            <Prompt when={isEntering} message={(location) => "are you sure you want to go to this loation!"}
+            />
+            <Card>
+                <form onFocus={formFocusHandler} className={classes.form} onSubmit={submitFormHandler}>
+                    {props.isLoading && (
+                        <div className={classes.loading}>
+                            <LoadingSpinner/>
+                        </div>
+                    )}
+
+                    <div className={classes.control}>
+                        <label htmlFor='author'>Author</label>
+                        <input type='text' id='author' ref={authorInputRef}/>
+                    </div>
+                    <div className={classes.control}>
+                        <label htmlFor='text'>Text</label>
+                        <textarea id='text' rows='5' ref={textInputRef}/>
+                    </div>
+                    <div className={classes.actions}>
+                        <button onClick={submitButtonHandler} className='btn'>Add Quote</button>
+                    </div>
+                </form>
+            </Card>
+        </Fragment>
+    );
 };
 
 export default QuoteForm;
