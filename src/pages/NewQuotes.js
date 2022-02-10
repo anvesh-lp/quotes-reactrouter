c94c1bfa8d86cfce99e1dc633d6ef29000198743
anvesh-lp <knanvesh.01@gmail.com>
2022-02-10 12:30:46 -0500

    Adding programmatic navigation

--- src/pages/NewQuotes.js
+++ src/pages/NewQuotes.js
@@ -1,8 +1,11 @@
 import QuoteForm from "../components/quotes/QuoteForm";
+import {useHistory} from "react-router-dom";
 
 const NewQuotes=()=>{
+    const history=useHistory();
     const quoteFormHandler = (data) => {
       console.log(data)
+        history.push("/quotes")
     }
     return (
         <QuoteForm onAddQuote={quoteFormHandler}/>
